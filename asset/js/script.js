c1cc545b01eda4122c3a3ac0866edddf00b6fd32
Eduardo Rocha Azevedo <142415927+Eduardo-Rocha-Azevedo@users.noreply.github.com>
2024-04-23 11:52:22 -0300

    slider

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,16 +1,32 @@
 const initSlider = () => {
-    const slideButtonn = document.querySelectorAll('.slide-button');
+    const slideButtons = document.querySelectorAll('.slide-button');
     const imageList = document.querySelector('.image-list');
-
-    //? Slide images according to the button clicks
-    slideButtonn.forEach(button =>{
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+    const sliderScrollbar = document.querySelector('.slider-scrollbar');
+    // Slide images according to the button clicks
+    slideButtons.forEach(button => {
         button.addEventListener('click', () => {
-            const direction = button.id ==='prev-slide' ? -1 : 1;
-            const scrollAmount = imageList.clientWidth * 2;
-            imageList.scrollBy({left:scrollAmount,behavior:'smooth'})
-        });   
+            const direction = button.id === 'prev-slide' ? -1 : 1;
+            const scrollAmount = direction * (imageList.clientWidth / 2); // Ajusta o valor do deslocamento conforme a direção
+            imageList.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+        });
     });
-}
 
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? 'none' : 'block';
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? 'none' : 'block';
+    };
+
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition  / maxScrollLeft) * ();
+    }
+
+    imageList.addEventListener('scroll', () => {
+        handleSlideButtons();
+        updateScrollThumbPosition();
+
+    });
+};
 
 window.addEventListener('load', initSlider);
